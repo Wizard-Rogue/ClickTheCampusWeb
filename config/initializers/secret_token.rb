606ed9e5053fe5e9c47efc7b481435430ee9d735
Joseph Barrozo <JABarrozO@gmail.com>
2013-02-11 09:53:09 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickTheCampus::Application.config.secret_token = '91c313ef523a9cc3304900e191deb8ef801ecb57e2294b25b7fbb36c50b8475e48b54735f4057a3b7f2dd9bbddd9fee48717f5d1569181a3d4bb5181c54c0c9f'
